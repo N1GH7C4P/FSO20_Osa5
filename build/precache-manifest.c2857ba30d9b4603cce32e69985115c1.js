feb0ecca246a41383319e17370a5dea3cc6079b5
N1GH7C4P <31281638+N1GH7C4P@users.noreply.github.com>
2020-05-28 19:02:25 +0300

    first commit

---
+++ build/precache-manifest.c2857ba30d9b4603cce32e69985115c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa0fac4d8a388adf7e74d412b41cb33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7735b026206ab6142c6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7735b026206ab6142c6",
+    "url": "/static/js/main.22bc202b.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
